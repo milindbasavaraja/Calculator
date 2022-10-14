d86e3ba99101252f92d9244971a6669ca0426e0b
milindbasavaraja <milindbasavaraja96@gmail.com>
2022-10-14 17:12:26 -0400

    Added Sum functionality

--- calculator.js
+++ calculator.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-  res.send("<h1> Calculator</h1>");
+  console.log(__dirname);
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+
+  var sum = num1 + num2;
+  res.send("The sum is " + sum);
 });
 
 app.listen(3000, () => {
